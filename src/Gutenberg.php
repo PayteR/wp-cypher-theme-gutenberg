5056fde03499cdc8ac210daf777d32b59c4032cb
Peter PayteR Gašparík <payter86@gmail.com>
2019-03-30 02:11:17 +0100

    folders refactoring

---
+++ src/Gutenberg.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cypher;
+
+/**
+ * Exists just to get absolute path in config for this package
+ *
+ * @author Peter "PayteR" Gašparík
+ * https://github.com/PayteR
+ * @copyright 2019
+ *
+ * Class Cypher
+ * @package Cypher
+ */
+class Gutenberg {
+
+}
